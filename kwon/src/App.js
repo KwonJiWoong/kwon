199a8d427ede45dc1782358fdaa8936fab8e576b
권지웅 <32256659+KwonJiWoong@users.noreply.github.com>
2021-06-01 07:45:13 +0900

    포트폴리오 수정 0601

--- kwon/src/App.js
+++ kwon/src/App.js
@@ -2,7 +2,11 @@ import './App.css';
 
 function Kwon() {
   return (
-    <div className="Kwon">
+    // 카드형 포폴 만들기 내 장단점
+    // 도전정신 - 이직, 공부, 어제보다 오늘 더 나아지는
+    // 창의적 - 잡코리아 HTML 코딩 시 단축키 동료들과 공유 및 계속 발전하려는 자세 / 이것보다 더 좋은 방법 늘 고민
+    <div className="kwon">
+      
     </div>
   );
 }
--- kwon/src/header.js
+++ kwon/src/header.js
@@ -1,47 +1,42 @@
+import { Component } from 'react';
 import './App.css';
-import React, { useState } from 'react';
 
-
-
-function Header() {
-    const [logoEvtTxt, logoEvtTest] = useState(0);
-    function nameAdd(user){
-        return user.firstName + user.lastName;
-    }
-    
-    //영문추가 시 고민중
-    const name = {
+class Header extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      name: {
         firstName : '권',
-        lastName: '지웅'
+        lastName : '지웅'
+      }
     }
-    
-    //로고 이벤트
-    function logoEvt(user){
-        const motion = nameAdd(user) + '의 포트폴리오 입니다.';
-        const motionTxt = motion.split("");
-        let motionResult;
-        let motionTxtIdx = 0;
-        var motionEvt = setInterval(motionRepeat, 100);
-        function motionRepeat(){
-            if(motionTxtIdx < motionTxt.length){
-                motionResult += motionTxt[motionTxtIdx];
-                console.log(motionResult);
-                motionTxtIdx++;
-            }
-            else{
-                clearInterval(motionEvt);
-                return motionResult;
-            }
-        }
-        return logoEvtTxt = motionResult;
+  }
+  render(){
+    function nameAdd(user){
+      return user.firstName + user.lastName;
     }
-  return (
-    <div className="header">
-      <h1 className="logo">
-      {logoEvt(name)}
-      </h1>
-    </div>
-  );
+    return (
+      <div className="header">
+        <ul className="skipNavigator">
+          <li><a href="#App">본문으로 이동</a></li>
+          <li><a href="#gnb">메뉴 이동</a></li>
+          <li><a href="#Footer">하단으로 이동</a></li>
+        </ul>
+        <h1 className="logo">
+          Kwon
+        </h1>
+        <h2 className="title">
+          {nameAdd(this.state.name)}의 포트폴리오 입니다.
+        </h2>
+        <ul id="gnb" className="gnb">
+          {/* 나중에 각 메뉴 별 작동 만들기 */}
+          <li><a href="#">메뉴1</a></li>
+          <li><a href="#">메뉴2</a></li>
+          <li><a href="#">메뉴3</a></li>
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default Header;
